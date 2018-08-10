75626fcac6587c583bb396fc9472f4c21978fa66
grabriel2323@gmail.com <grabriel2323@gmail.com>
2018-08-10 11:09:28 -0300

    video 10 nao terminado

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -95,6 +95,7 @@ public class PlayerCtrl : MonoBehaviour {
 	void Jump(){
 		if (isGrounded){
 			isJumping = true;
+			AudioManager.instance.PlayJumpSound(gameObject);
 			rb.AddForce(new Vector2(0f, jumpSpeed));
 			anim.SetInteger("State 0", 1);
 
@@ -103,6 +104,7 @@ public class PlayerCtrl : MonoBehaviour {
 
 		if (canDoubleJump && !isGrounded){
 			rb.velocity = Vector2.zero;
+			AudioManager.instance.PlayJumpSound(gameObject);
 			rb.AddForce(new Vector2(0f, jumpSpeed));
 			anim.SetInteger("State 0", 1);
 			canDoubleJump = false;
@@ -121,6 +123,7 @@ public class PlayerCtrl : MonoBehaviour {
 
 		void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.CompareTag("Coin")){
+			AudioManager.instance.PlayCoinPickupSound(other.gameObject);
 			SFXManager.instance.ShowCoinParticles(other.gameObject);
 			Destroy(other.gameObject);
 		}
