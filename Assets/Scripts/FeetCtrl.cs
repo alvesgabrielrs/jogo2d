582695644aef0e4dd7a50141e32d1face7309895
Gabriel Rodrigues <grabriel2323@gmail.com>
2018-08-14 10:16:53 -0300

    MovingPlatform parte 2 - corrigindo parent

---
+++ Assets/Scripts/FeetCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetCtrl : MonoBehaviour {
+
+	GameObject player;
+
+	void Start () {
+		player = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag("MovingPlatform")){
+			player.transform.parent = other.transform.parent.transform;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.CompareTag("MovingPlatform")){
+			player.transform.parent = null;
+		}
+	}
+}
